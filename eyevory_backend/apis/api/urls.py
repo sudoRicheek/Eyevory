36f384e2d5fb8541e17ddbb57630f6e16009c2a1
Rahul Prajapat <rickey26photon@gmail.com>
2022-04-29 18:10:05 +0530

    updated user classes

--- eyevory_backend/apis/api/urls.py
+++ eyevory_backend/apis/api/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from apis.api.views import (
 	RegisterView,
 	get_profile,
+	list_nodes,
+	add_node,
+	delete_node
 	# update_profile,
 	# ChangePasswordView,
 	# get_pastchecks,
@@ -11,8 +14,10 @@ from apis.api.views import (
 app_name = 'apis'
 urlpatterns = [
 	path('register/', RegisterView, name='auth_register'),
-    # path('signup/', registration_view, name='user-api-signup'),
     path('profile/', get_profile, name='user-api-profile-view'),
+	path('addnode/', add_node, name='user-add-node'),
+	path('deletenode/', delete_node, name='user-delete-node'),
+	path('listnodes/', list_nodes, name='available-nodes')
     # path('update/', update_profile, name='account-api-profile-update'),
 	# path('upassword/', ChangePasswordView.as_view(), name='account-api-change-password'),
 	# path('pastchecks/', get_pastchecks, name='account-api-get-pastchecks')
--- eyevory_backend/apis/api/views.py
+++ eyevory_backend/apis/api/views.py
@@ -1,3 +1,4 @@
+from django import http
 from rest_framework import status, generics, serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -13,8 +14,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 # from django.utils import timezone
 
-from apis.models import Profile
-from apis.api.serializers import ProfileSerializer, RegistrationSerializer
+from apis.models import Profile, Node
+from apis.api.serializers import NodeSerializer, ProfileSerializer, RegistrationSerializer
 
 import os
 from pytz import timezone
@@ -59,19 +60,52 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 ###################################################################
 
-@api_view(['GET', ])
-def get_profile(request):
-    pass
 
-
+@api_view(['POST', ])
+def login(request):
+    if request.method == "POST":
+        pass
+###################################################################
 
 # Get Profile Details view
-#@api_view(['GET', ])
-#def get_profile(request):
- #   if request.method == "GET":
-  #      profile = get_object_or_404(Profile, username=request.user)
-   #     pr_serializer = ProfileSerializer(profile)
-    #    data = pr_serializer.data
-     #   data['username'] = profile.username
-      #  return Response(data)
+@api_view(['GET', ])
+def get_profile(request):
+    if request.method == "GET":
+        profile = get_object_or_404(Profile, username=request.user)
+        pr_serializer = ProfileSerializer(profile)
+        data = pr_serializer.data
+        data['username'] = profile.username
+        return Response(data)
 ###################################################################
+
+
+
+# Get all nodes
+@api_view(['GET', ])
+def list_nodes(request):
+    if request.method == "GET":
+        return Response(Node.objects.all())
+###################################################################
+
+
+
+# Add a node
+@api_view(['POST', ])
+def add_node(request):
+    if request.method == "POST":
+        serializer = NodeSerializer(data = request.data)
+        serializer.add()
+        return Response(status=status.HTTP_201_CREATED)
+###################################################################
+
+
+# Delete a node
+@api_view(['DELETE, '])
+def delete_node(request):
+    if request.method == "DELETE":
+        serializer = NodeSerializer(data = request.data)
+        serializer.remove()
+        return Response(status=status.HTTP_200_OK)
+###################################################################
+
+
